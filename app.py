c1d746af82f34462a86f00028439756d4e671d3e
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-08 10:42:16 +0200

    Day014 - done. Added database and few positions into database.

--- app.py
+++ app.py
@@ -1,26 +1,60 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-names = {"Wojtek": {"age": 26, "gender": "male", "has_tattoos": True},
-         "Ania": {"age": 25, "gender": "female", "has_tattoos": True}}
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
 
-videos = {}
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+# db.create_all()     #run this only once!
+
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
 
-class Video(Resource):
-    def get(self, video_id):
-        return videos[video_id]
+resource_fields = {
+    "id": fields.Integer,
+    "name": fields.String,
+    "views": fields.Integer,
+    "likes": fields.Integer
+}
 
+class Video(Resource):
+    @marshal_with(resource_fields)
+    def get(self, video_id):
+        result = VideoModel.query.filter_by(id=video_id).first()
+        return result
+
+    @marshal_with(resource_fields)
     def put(self, video_id):
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video id taken...")
+
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return "", 204
+
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
+data = [{"likes": 90, "name": "Jackass", "views": 100},
+        {"likes": 100, "name": "How to make REST API", "views": 8000000},
+        {"likes": 1, "name": "Odyn", "views": 740000}]
 
-response = requests.put(BASE + "video/1", {"likes": 10, "name": "Titanic", "views": 1000000})
-print(response.json())
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
 
 input()
-
-response = requests.put(BASE + "video/1")
+response = requests.get(BASE + "video/2")
 print(response.json())
